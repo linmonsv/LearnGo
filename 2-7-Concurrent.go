3dade75adcb3ad3bb5fea5c809383e9ce7a1c094
qin2 <qin2@qq.com>
2016-01-08 21:47:13 +0800

    add select and so on

--- 2-7-Concurrent.go
+++ 2-7-Concurrent.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"time"
 )
 
 func say(s string) {
@@ -55,8 +56,91 @@ func example_Buffered_Channels() {
 	fmt.Println(<-c)
 }
 
+func fibonacci(n int, c chan int) {
+	x, y := 1, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	close(c)
+}
+
+func exmaple_range_close() {
+	fmt.Println("exmaple_range_close")
+	c := make(chan int, 10)
+	go fibonacci(cap(c), c)
+	for i := range c {
+		fmt.Println(i)
+	}
+}
+
+func fibonacci2(c, quit chan int) {
+	x, y := 1, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		default:
+			//当监听的channel都没有准备好的时候，默认执行的（select不再阻塞等待channel）
+		}
+	}
+}
+
+func exmaple_select() {
+	fmt.Println("exmaple_select")
+	c := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+	fibonacci2(c, quit)
+}
+
+func example_timeout() {
+	fmt.Println("example_timeout")
+	c := make(chan int)
+	o := make(chan bool)
+	go func() {
+		for {
+			select {
+			case v := <-c:
+				println(v)
+			case <-time.After(5 * time.Second):
+				println("timeout")
+				o <- true
+				break
+			}
+		}
+	}()
+	<-o
+}
+
 func main() {
 	exmaple_goroutine()
 	exmaple_channel()
 	example_Buffered_Channels()
+	exmaple_range_close()
+	exmaple_select()
+	example_timeout()
 }
+
+/*
+runtime goroutine
+runtime包中有几个处理goroutine的函数：
+Goexit
+退出当前执行的goroutine，但是defer函数还会继续调用
+Gosched
+让出当前goroutine的执行权限，调度器安排其他等待的任务运行，并在下次某个时候从该位置恢复执行。
+NumCPU
+返回 CPU 核数量
+NumGoroutine
+返回正在执 行和排队的任务总数
+GOMAXPROCS
+用来设置可以运行的CPU核数
+*/
